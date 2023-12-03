d14c047cfd1c572209b57776f89938c76952e166
markleung0905 <markleung0905@gmail.com>
2023-12-03 17:16:46 +0000

    bug  fix for multiple domain

--- src/updatedns.py
+++ src/updatedns.py
@@ -18,21 +18,23 @@ publicIP = pif.get_public_ip('ident.me')
 domain = os.getenv('DOMAIN')
 names = os.getenv('NAME').split(',')
 
-for name in names:
-    if os.path.isfile('godaddy_ip.txt'):
-        try:
-            ip_file = open('godaddy_ip.txt', 'r')
-            read_ip = ip_file.read().strip('\n')
-            ip_file.close()
-        except:
-            print("Cannot read IP file")
-            sys.exit()
-        if read_ip == publicIP:
-            print("Read the IP file, no need to change IP")
-            sys.exit()
 
-    # Try to retrieve the record and update it if necessary 
+if os.path.isfile('godaddy_ip.txt'):
     try:
+        ip_file = open('godaddy_ip.txt', 'r')
+        read_ip = ip_file.read().strip('\n')
+        ip_file.close()
+    except:
+        print("Cannot read IP file")
+        sys.exit()
+    if read_ip == publicIP:
+        print("Read the IP file, no need to change IP")
+        sys.exit()
+
+
+# Try to retrieve the record and update it if necessary 
+try:
+    for name in names:
         currentIP = userClient.get_records(domain, record_type='A', name=name)
         if (publicIP != currentIP[0]["data"]):
             updateResult = userClient.update_record_ip(publicIP, domain, name, 'A')
@@ -43,6 +45,6 @@ for name in names:
                 print('Updated DNS record and wrote IP file.')
         else:
             print('Checked the DNS record, no update needed.')
-    except:
-        print(sys.exc_info()[1])
-        sys.exit()
+except:
+    print(sys.exc_info()[1])
+    sys.exit()
